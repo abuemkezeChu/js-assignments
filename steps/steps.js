1920c1751ba46d39e1a382c5cab6213b46ce351e
Abuemkeze Chu <abuemkeze@gmail.com>
2023-08-16 14:39:05 +0100

    feat: code to print a pattern of steps

---
+++ steps/steps.js
@@ -0,0 +1,12 @@
+const prompt = require('prompt-sync')();
+
+let stepPattern = function() {
+  let steps = " "
+let numberOfSteps = prompt('Enter the number of steps: ')
+
+// For each iteration, the value of steps defined above is updated
+for(rows = 0; rows < numberOfSteps; rows++){
+  steps += "* ";
+  console.log(steps);
+}
+}
